6aab9a32004b36a1766f1a83644dfdf21704a524
Emmzzo <sibo2540@gmail.com>
2018-05-12 23:13:38 +0200

    add files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php>
+
+/*$mult=array(1,2,3,4,5,6,7,8,9,10);
+$result=array();
+for($i=1;$i<10;$i++){
+
+$result[$i]=$mult[$i]*5;
+}
+foreach($result as $res){
+
+echo"5*"$res"="5*$res;
+} */
+function sum(){
+
+    echo "the sum of two number is:"12+4;
+}
+sum();
+?>
